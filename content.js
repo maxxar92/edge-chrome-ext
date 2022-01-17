93cd2fc40d8da5d9f1c0b5a912f12fef6aff8f33
maxxar92 <max.reimann@hpi.de>
2022-01-17 15:27:55 +0100

    Use v1 public host API. Also add prettierrc formatting

--- content.js
+++ content.js
@@ -1,66 +1,77 @@
 function in_srcset(element, srcUri) {
-	if ($(element).attr('srcset') !== undefined) {
-		return $(element).attr('srcset').split(',').some(urlsub=>
-			srcUri.includes(urlsub.trim().split(' ')[0]))
-	}
+  if ($(element).attr('srcset') !== undefined) {
+    return $(element)
+      .attr('srcset')
+      .split(',')
+      .some((urlsub) => srcUri.includes(urlsub.trim().split(' ')[0]))
+  }
 
-	return false;
+  return false
 }
 
 function wrap(req) {
-	var handled=false;
-	$("img").each(function(index, element) {
-		var srcUri=decodeURI(req.requesturl);
-		if (srcUri.includes($(element).attr('src')) || in_srcset(element, srcUri)) {
-			console.log(element);
-			var device = req.device_id;
-			var name = req.host_info["host_name"];
-			var location = req.host_info["location"];
-			var hosttext = '<div class="text-block"><p>' + name + " (" + location + ")</p> </div>";
-			var tags = $(element).parents().map(function() { return this.tagName; }).get();
-			if (tags.some(el=>el==="A")) { // don't wrap if parent is a link, as this would hide the image somehow
-				$(element).after(hosttext);
-			} else {
-				$(element).wrap('<div class="host-img-container"></div>').after(hosttext);
-			}
-			handled=true;
-		}
-	});
+  var handled = false
+  $('img').each(function (index, element) {
+    var srcUri = decodeURI(req.requesturl)
+    if (srcUri.includes($(element).attr('src')) || in_srcset(element, srcUri)) {
+      var name = req.host_info['deviceName']
+      var city = req.host_info['city'] == '' ? 'unknown' : req.host_info['city']
+      var location = city + '-' + req.host_info['country']
+      var hosttext =
+        '<div class="text-block"><p>' + name + ' (' + location + ')</p> </div>'
+      var tags = $(element)
+        .parents()
+        .map(function () {
+          return this.tagName
+        })
+        .get()
+      if (tags.some((el) => el === 'A')) {
+        // don't wrap if parent is a link, as this would hide the image somehow
+        $(element).after(hosttext)
+      } else {
+        $(element)
+          .wrap('<div class="host-img-container"></div>')
+          .after(hosttext)
+      }
+      handled = true
+    }
+  })
 
-	return handled;
+  return handled
 }
 
+var unhandled = []
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.device_id !== null && message.requesturl !== undefined) {
+    var handled = wrap(message)
+    if (!handled) {
+      unhandled.push(message)
+    }
+    sendResponse(true)
+  }
+})
 
-var unhandled = [];
-chrome.runtime.onMessage.addListener( (message, sender, sendResponse) => { 
-	if (message.device_id !== null && message.requesturl !== undefined) {
-		console.log(message.requesturl);
-			var handled = wrap(message);
+window.addEventListener(
+  'load',
+  function load(event) {
+    window.removeEventListener('load', load, false)
+    //enter here the action you want to do once loaded
+    // try to wrap unhandled images
+    for (let i = unhandled.length - 1; i >= 0; i--) {
+      if (wrap(unhandled[i])) {
+        unhandled.splice(i, 1)
+      }
+    }
+  },
+  false
+)
 
-			if (!handled) {
-				unhandled.push(message)
-			}
-
-		sendResponse(true);
-	}
-});
-
-window.addEventListener("load", function load(event){
-    window.removeEventListener("load", load, false); 
-    //enter here the action you want to do once loaded 
-    	// try to wrap unhandled images
-		for (let i = unhandled.length - 1; i >= 0; i--) {
-		  if (wrap(unhandled[i])) {
-		    unhandled.splice(i, 1);
-		  }
-		}
-},false);
-
-$( document ).ready(function() {
+$(document).ready(function () {
   // Handler for .ready() called.
-  $("<style>")
-    .prop("type", "text/css")
-    .html(`.text-block {
+  $('<style>')
+    .prop('type', 'text/css')
+    .html(
+      `.text-block {
 	  position: absolute;
 	  text-align: center;
 	  top: 0%;
@@ -73,7 +84,7 @@ $( document ).ready(function() {
 		position: relative;
 	}
 
-	`)
-    .appendTo("head");
-});
-
+	`
+    )
+    .appendTo('head')
+})
--- getHeaders.js
+++ getHeaders.js
@@ -1,40 +1,57 @@
-var hosts = null;
+var hosts = null
 
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myArr = JSON.parse(this.responseText);
-        hosts = myArr;
-    }
-};
-var url = "http://edge-analytics.org/api/allhosts";
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
+var xmlhttp = new XMLHttpRequest()
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myArr = JSON.parse(this.responseText)
+    hosts = myArr
+  }
+}
+var url = 'https://public-api.edge.network/devices'
+xmlhttp.open('GET', url, true)
+xmlhttp.setRequestHeader(
+  'Authorization',
+  'Bearer J3PSTRRW74YZNWZ4FZWSUCJSPPFV56VE'
+)
+xmlhttp.send()
 
 function get_device(details) {
-	var responseHeaders = details.responseHeaders;
-	if (responseHeaders) {
-		for (var i = 0; i < responseHeaders.length; i++) {
-	      var responseHeader = responseHeaders[i];
-	      if (responseHeader.name === "edge-device") {
-	      	return responseHeader.value;
-	      }
-    	}
-    } 
+  var responseHeaders = details.responseHeaders
+  if (responseHeaders) {
+    for (var i = 0; i < responseHeaders.length; i++) {
+      var responseHeader = responseHeaders[i]
+      if (responseHeader.name === 'edge-device') {
+        return responseHeader.value
+      }
+    }
+  }
 
-    return null;
+  return null
 }
 
 function onHeadersReceivedCallback(details) {
-	chrome.extension.getBackgroundPage().console.log(details);
-	var device_id = get_device(details);
-	var host_info = hosts[device_id];
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    	chrome.tabs.sendMessage(tabs[0].id, {requesturl: details.url, details: details, device_id: device_id, host_info: host_info}, function(response) {});  
-	});
-
+  chrome.extension.getBackgroundPage().console.log(details)
+  var device_id = get_device(details)
+  var host_info = hosts.filter((i) => i.networkid === device_id)
+  if (host_info.length > 0) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        {
+          requesturl: details.url,
+          details: details,
+          device_id: device_id,
+          host_info: host_info[0],
+        },
+        function (response) {}
+      )
+    })
+  }
 }
 
-chrome.webRequest.onHeadersReceived.addListener(onHeadersReceivedCallback, 
-	{urls: ["https://cdn.edge.network/*","https://img.monocle.com/*"]}, ["blocking", "responseHeaders"]);
+// only listen to specified domains we know to serve edge cdn images
+chrome.webRequest.onHeadersReceived.addListener(
+  onHeadersReceivedCallback,
+  { urls: ['https://cdn.edge.network/*', 'https://img.monocle.com/*'] },
+  ['blocking', 'responseHeaders']
+)
